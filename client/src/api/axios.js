34af2b27cbaa390df469ce9bb7fb1ff30dae2c42
nafkota <hiwotmelaku84@yahoo.com>
2025-06-04 16:24:55 -0400

    some fixes

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-  // baseURL: "http://127.0.0.1:5001/fir-d7510/us-central1/api",
-  // baseURL: "http://localhost:5000",
-  baseURL: "https://amazon-back-end-q55o.onrender.com/",
+  baseURL: "https://amazon-backend-ut59.onrender.com",
 });
 export { axiosInstance };
